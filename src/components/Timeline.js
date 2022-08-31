9069242a906e8c920d1a0ef18b4f4e5c786246d6
giovanniPepi <gtpepi@hotmail.com>
2022-08-31 11:15:03 -0300

    Update post getter to store posts as an array in state

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,28 +2,35 @@ import { collection, getDocs } from "firebase/firestore";
 import { useState } from "react";
 import { useEffect } from "react";
 import { db } from "../firebase";
+import uniqid from "uniqid";
 
 const Timeline = () => {
   const [posts, setPosts] = useState([]);
+
   useEffect(() => {
-    const postArray = [];
     const getPostFromFirestore = async () => {
       const colRef = collection(db, "postDB");
       const docs = await getDocs(colRef);
       docs.forEach((doc) => {
-        postArray.push({ ...doc.data() });
+        // https://javascript.plainenglish.io/how-to-add-to-an-array-in-react-state-3d08ddb2e1dc
+        setPosts((posts) => [...posts, doc.data()]);
       });
     };
 
     getPostFromFirestore();
-    setPosts(postArray);
-
-    console.log(postArray);
   }, []);
 
   console.log(posts);
 
-  return <section className="timeline">timeline</section>;
+  return (
+    <section className="timeline">
+      <div>
+        {posts.map((post) => {
+          return <div key={uniqid()}>{post.id}</div>;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Timeline;
