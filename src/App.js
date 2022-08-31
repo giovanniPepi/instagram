bba0cdfdfb0bb4c05fbaf214fa50630a93924cf0
giovanniPepi <gtpepi@hotmail.com>
2022-08-31 09:49:47 -0300

    Add function to get posts from firestore

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import Header from "./components/Header";
 import RightSide from "./components/RightSide";
 import Timeline from "./components/Timeline";
+import getPostFromFirestore from "./functions/getPostFromFirestore";
 import postToFirestore from "./functions/postToFirestore";
 
 const App = () => {
-  postToFirestore();
+  // postToFirestore();
+  // getPostFromFirestore();
   return (
     <main className="app">
       <Header />
--- src/functions/getPostFromFirestore.js
+++ src/functions/getPostFromFirestore.js
@@ -1,3 +1,12 @@
-const getPostFromFirestore = () => {};
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+
+const getPostFromFirestore = async () => {
+  const colRef = collection(db, "postDB");
+  const docs = await getDocs(colRef);
+  docs.forEach((doc) => {
+    console.log(doc.data());
+  });
+};
 
 export default getPostFromFirestore;
