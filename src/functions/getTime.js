b5bd1214a57a2dde0b07f5109cac56c80672543b
a4 <gtpepi@hotmail.com>
2022-08-31 19:18:34 -0300

    tweak gettime

--- src/functions/getTime.js
+++ src/functions/getTime.js
@@ -5,19 +5,36 @@ const getTime = (unix) => {
 
   const dateDiffInDays = (a, b) => {
     // Discard the time and time-zone information.
-    const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
-    const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+    const utc1 = Date.UTC(
+      a.getFullYear(),
+      a.getMonth(),
+      a.getDate(),
+      a.getHours()
+    );
+    const utc2 = Date.UTC(
+      b.getFullYear(),
+      b.getMonth(),
+      b.getDate(),
+      b.getHours()
+    );
 
-    return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+    return (utc2 - utc1) / _MS_PER_DAY;
   };
 
-  const a = new Date(unix),
-    b = new Date(),
-    difference = dateDiffInDays(a, b);
-
-  console.log(a, b);
-
-  return difference;
+  const a = new Date(unix);
+  const b = new Date();
+  const difference = dateDiffInDays(a, b);
+  console.log(difference);
+  if (difference <= 0.0833) return `${difference * 24} hour`;
+  else if (difference > 0.0833 && difference <= 1)
+    return `${difference * 24} hours`;
+  else if (difference > 1 && difference <= 2)
+    return `${difference.toFixed(0)} day`;
+  else if (difference > 1 && difference <= 7)
+    return `${difference.toFixed(0)} days`;
+  else if (difference > 7 && difference <= 14)
+    return `${(difference / 7).toFixed(0)} week`;
+  else if (difference > 7) return `${(difference / 7).toFixed(0)} weeks`;
 };
 
 export default getTime;
--- src/functions/getTime.test.js
+++ src/functions/getTime.test.js
@@ -1,8 +1,17 @@
 import getTime from "./getTime";
 
 test("returns 2431 days between 04/01/2016 and 31/08/2022", () => {
-  expect(getTime(1451907936 * 1000)).toBe(2431);
+  expect(getTime(1451907936 * 1000)).toBe("347 weeks");
 });
-test("returns days between 23/08 and 31/08/2022", () => {
-  expect(getTime(1661258701 * 1000)).toBe(8);
+test("returns 1 week", () => {
+  expect(getTime(1661258701 * 1000)).toBe("1 week");
+});
+test("returns hours", () => {
+  expect(getTime(1661979572 * 1000)).toBe("2 hours");
+});
+test("returns hour", () => {
+  expect(getTime(1661979972 * 1000)).toBe("1 hour");
+});
+test("returns day", () => {
+  expect(getTime(1661897172 * 1000)).toBe("1 day");
 });
