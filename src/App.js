e81d7d419ba51af00d840f8454fdf5f1466bf847
giovanniPepi <gtpepi@hotmail.com>
2022-08-31 10:20:31 -0300

    Move getPosts to Timeline module

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import "./App.css";
 import Header from "./components/Header";
 import RightSide from "./components/RightSide";
 import Timeline from "./components/Timeline";
-import getPostFromFirestore from "./functions/getPostFromFirestore";
-import postToFirestore from "./functions/postToFirestore";
 
 const App = () => {
-  // postToFirestore();
-  // getPostFromFirestore();
   return (
     <main className="app">
       <Header />
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,4 +1,28 @@
+import { collection, getDocs } from "firebase/firestore";
+import { useState } from "react";
+import { useEffect } from "react";
+import { db } from "../firebase";
+
 const Timeline = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const postArray = [];
+    const getPostFromFirestore = async () => {
+      const colRef = collection(db, "postDB");
+      const docs = await getDocs(colRef);
+      docs.forEach((doc) => {
+        postArray.push({ ...doc.data() });
+      });
+    };
+
+    getPostFromFirestore();
+    setPosts(postArray);
+
+    console.log(postArray);
+  }, []);
+
+  console.log(posts);
+
   return <section className="timeline">timeline</section>;
 };
 
